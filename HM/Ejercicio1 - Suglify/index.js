ee9302d3bb9e08c3b44d3e1c947431eae8991186
Nico2663 <48460617+Nico2663@users.noreply.github.com>
2020-09-21 14:04:52 -0300

    Exercice 2 ended

---
+++ HM/Ejercicio1 - Suglify/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+const slugify = require('slugify')
+
+
+const server = http.createServer(function(req,res){
+    console.log("Alguien accedió al servidor");
+    var cadena = "¡Vamo arriba Uruguay & España! �";
+    console.log(slugify(cadena));
+    res.end("Respuesta");
+});
+
+server.listen(8080);
