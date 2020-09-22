76eae0b89a881431d317333920da52812d5056d1
Nico2663 <48460617+Nico2663@users.noreply.github.com>
2020-09-22 13:23:24 -0300

    Files fixed

---
+++ HM/Ejercicio5-7 Argumentos/index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const fs = require("fs");
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.json(persona);
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
