c8263d1e1e866d87b86e8a870b776b22813bd4cc
Nico2663 <48460617+Nico2663@users.noreply.github.com>
2020-09-22 15:17:35 -0300

    finished filter

--- HM/Ejercicio5-7 Argumentos/index.js
+++ HM/Ejercicio5-7 Argumentos/index.js
@@ -2,11 +2,41 @@ const http = require("http");
 const fs = require("fs");
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 8000;
+const path = require('path');
 
 app.get('/', (req, res) => {
   res.json(persona);
 });
+
+let routeFile = __dirname;
+let file_extension = process.argv[1];
+// let file_extension = process.argv[1];
+
+function fromDir(startPath,filter){
+
+  //console.log('Starting from dir '+startPath+'/');
+
+  if (!fs.existsSync(startPath)){
+      console.log("no dir ",startPath);
+      return;
+  }
+
+  var files=fs.readdirSync(startPath);
+  for(var i=0;i<files.length;i++){
+      var filename=path.join(startPath,files[i]);
+      var stat = fs.lstatSync(filename);
+      if (stat.isDirectory()){
+          fromDir(filename,filter); //recurse
+      }
+      else if (filename.indexOf(filter)>=0) {
+          console.log('-- found: ',filename);
+      };
+  };
+};
+
+
+fromDir(routeFile,file_extension);
 
 
 app.listen(port, () => {
