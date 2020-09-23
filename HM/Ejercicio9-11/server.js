a305bd10d0c177b7a59a9a4294c26c8aed5fb587
Nico2663 <48460617+Nico2663@users.noreply.github.com>
2020-09-23 15:15:52 -0300

    Ejercicio 10 finalizado

--- HM/Ejercicio9-11/server.js
+++ HM/Ejercicio9-11/server.js
@@ -2,20 +2,50 @@ const http = require("http");
 const express = require('express')
 const app = express()
 const port = 3000;
+var dias_semana = new Array ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+var dias_fin_semana = new Array ( "Saturday","Sunday",);
+
+var dateTime = require('node-datetime');
+var dt = dateTime.create();
+var formatted = dt.format('W');
+console.log(formatted);
+var sItem = 'SAMPLE ITEM';
+var sample_list = [sItem, sItem, sItem, sItem];
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/home.html')
+    for (let i = 0; i < dias_semana.length; i++) {
+        if(dias_semana[i] == formatted){
+            res.render('pages/home' , {title: "Hoy es un día de semana"});
+        }
+    }
+    for (let i = 0; i < dias_fin_semana.length; i++) {
+        if(dias_fin_semana[i] == formatted){
+            res.render('pages/home' , {title: "Hoy es fin de semana"});
+        }
+    }
 });
 
 
-// // set the view engine to ejs
-// app.set('view engine', 'ejs');
+// use res.render to load up an ejs view file
 
-// // use res.render to load up an ejs view file
+// about page
+app.get('/productos', function(req, res) {
+    res.render('pages/productos', { products: sample_list });
+});
+app.get('/sobre-nosotros', function(req, res) {
+    res.render('pages/index');
+});
+app.get('/contacto', function(req, res) {
+    res.render('pages/index');
+});
 
-// // index page 
+app.listen(3000);
+
+
+
+
 // app.get('/', function(req, res) {
 //     var mascots = [
 //         { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
@@ -29,13 +59,5 @@ app.get('/', (req, res) => {
 //         tagline: tagline
 //     });
 // });
-
-// // about page
-// app.get('/about', function(req, res) {
-//     res.render('pages/about');
-// });
-
-// app.listen(8080);
-// console.log('8080 is the magic port');
 
 
