db85185733639cd93f6c3e64ee11feceff112681
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-24 18:05:27 -0300

    Ejercicio 14 finalizado

--- HM/Ejercicio10-14 express-json/index.js
+++ HM/Ejercicio10-14 express-json/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const port = 3000;
 require('./routes')(app);
 app.use(express.static("public"));
--- HM/Ejercicio10-14 express-json/routes.js
+++ HM/Ejercicio10-14 express-json/routes.js
@@ -1,18 +1,39 @@
 module.exports= function(app) {
+  const db = require('./db');
 
   app.get('/teams', function(req,res){
-    let teams = require('./db');
-    res.send(teams);
+    res.send(db);
   });
 
-  app.get('/teams:id', function(req,res){
-    let teams = require('./db');
-    var id = req.params.id;
-    for (let i = 0; i < teams.length; i++) {
-      console.log(teams[i].id);
-      
+  app.get('/teams/:id', function(req,res){
+    const id = req.params.id;
+    db.teams.hasOwnPeoperty(id)
+      ? res.status(200).json(db.teams[id])
+      : res.status(404).send("404 error");
+  });
+
+  app.post('/teams', (req,res) => {
+    let nuevoPais = req.body.id;
+    if(db.teams.hasOwnPeoperty(nuevoPais)){
+      res.status(409).json('Usuario ya existe');
+    }else {
+      db.teams[nuevoPais] = req.body;
+      res.status(200).json(db);
+    }    
+  });
+
+  app.delete('/teams/:id', function(req,res){
+    const id = req.params.id;
+    delete db.teams[id];
+  });
+
+  app.patch('/teams/:id', function(req,res){
+    const id = req.params.id;
+    const dataToadd = req.body;
+    const propsTomodify = Object.keys(dataToadd);
+    if(db.teams.hasOwnPeoperty(id)){
+      delete dataToadd[id];
     }
-    
+    db.teams[id] = {...db.teams[id], ...dataToadd}
   });
-
 }
