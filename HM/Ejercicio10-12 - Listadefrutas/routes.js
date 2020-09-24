6c40aab39c5b1d6ad917dc649b8dc09eedc5c37a
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-24 13:35:37 -0300

    Se agrega material de estudio

--- HM/Ejercicio10-12 - Listadefrutas/routes.js
+++ HM/Ejercicio10-12 - Listadefrutas/routes.js
@@ -8,6 +8,7 @@ module.exports= function(app) {
   });
 
   app.post('/frutas', (req, res) => {
-    res.render('pages/frutas')
+    // res.render('pages/frutas')''
+    console.log(req.body);
   });
 }
