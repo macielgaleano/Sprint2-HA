4a66209beda2b0454912aa4dff225c1277ec82ec
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-26 20:14:28 -0300

    Axios porst and axios get

--- Axios/index.js
+++ Axios/index.js
@@ -1,22 +1,25 @@
 const axios = require('axios').default;
 
 function getData() {
-    axios.get("https://reqres.in/api/users/2").then((response) => {
-    //   console.log(response);
-    });
+    axios
+        .get("https://reqres.in/api/users/2").then((response) => {
+        //console.log(response);
+        }
+    );
 }
 
 function setData(){
-    axios.post("https://reqres.in/api/users/register", {
-        email: 'macielgaleano@gmail.com',
-        password: 'pistol'
-    })
-    .then(res => {
-        console.log(res)
-    })
-    .catch(err => {
-        console.error(err); 
-    })
+    axios
+        .post("https://reqres.in/api/users/register", {
+            email: 'macielgaleano@gmail.com',
+            password: 'pistol'
+        })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.error(err); 
+        })
 } 
 
 getData();
