9694c0b29eb3a8831bfe23a88f1713067c4878b4
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-28 16:20:11 -0300

    no me anda

--- HM/Ejercicio16-Blog/routes.js
+++ HM/Ejercicio16-Blog/routes.js
@@ -1,6 +1,5 @@
 module.exports = function(app){
   const url = 'https://gist.githubusercontent.com/SuecoMarcus/440d8206cbf0e3e9491d0c431c0e19b6/raw/cba8bb296345af148ff5e646bbc5d76900e4e4d7/posts.json';
-  const axios = require('axios');
   global.fetch = require("node-fetch");
 
   let articles_home = [];
--- HM/Ejercicio20 -Mysql/routes.js
+++ HM/Ejercicio20 -Mysql/routes.js
@@ -1,10 +1,40 @@
 module.exports = function(app){
+
+  const mysql = require('mysql');
+  let usersSaved = [];
+  
+  const conection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+
+  conection.connect(function(err){
+    if(err) throw err;
+    console.log('DB conncected!🐦')
+  });
+
   app.get('/', (req, res) => {
     res.send('home');
   });
 
   app.get('/usuarios', (req, res) => {
-    res.render('pages/usuarios')
+    conection.query('select * from users', async(err, users) => {
+      if(err) throw err;
+      await users.map((item,index,array) => {
+        usersSaved.push({
+          id: item.id,
+          firstname: item.firstname,
+          lastname: item.lastname,
+          age: item.age
+        })
+      })
+      console.log(usersSaved);
+    })
+    conection.end();
+    res.render('pages/usuarios', {users: usersSaved})
+    
   })
 
   app.post('/usuarios', (req, res) => {
@@ -12,11 +42,16 @@ module.exports = function(app){
   })
 
   app.get('/usuarios/eliminar/:id', (req, res) => {
-    res.render('pages/home')
+    conection.query('delete from users where id =' + conection.escape(req.params.id), (err, result) => {
+      if(err) throw err;
+      console.log(result);
+    })
+    conection.end();
+    res.redirect('/usuarios')
   })
 
-  app.post('/usuarios/modificar', (req, res) => {
-    res.render('pages/home')
+  app.post('/usuarios/modificar' , (req, res) => {
+    
   })
 
 
