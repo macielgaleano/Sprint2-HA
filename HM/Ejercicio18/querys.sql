14f6b7d31840463f751c7e6ac06b1e7446e3e6f9
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-28 14:30:45 -0300

    Create querys.sql

---
+++ HM/Ejercicio18/querys.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `usuarios` (  
+`id` int (11) AUTO_INCREMENT,  
+`nombre` varchar (100) DEFAULT NULL,  
+`apellido` varchar (100) DEFAULT NULL,  
+`edad` int (11), 
+`password` varchar (150) DEFAULT NULL,  
+PRIMARY KEY (`id`)
+);
