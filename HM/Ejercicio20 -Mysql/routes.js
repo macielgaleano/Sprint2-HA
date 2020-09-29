2ed6d0ff8998eaac54824a1ef83e65ea92a71fa5
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-09-29 11:41:36 -0300

    Using parley

--- HM/Ejercicio20 -Mysql/routes.js
+++ HM/Ejercicio20 -Mysql/routes.js
@@ -2,6 +2,7 @@ module.exports = function(app){
 
   const mysql = require('mysql');
   const axios = require('axios');
+  const { body,check, validationResult } = require('express-validator');
   
   const connection = mysql.createConnection({
     host: 'localhost',
@@ -39,13 +40,24 @@ module.exports = function(app){
     
   })
 
-  app.post('/usuarios', (req, res) => {
+  app.post('/usuarios',[
+    //Comprabate sizes
+    body('nombre').not().isEmpty().trim().escape(),
+    body('apellido').not().isEmpty().trim().escape(),
+    body('nombre').isLength({ min: 3 }).withMessage('El nombre requiere mas de 3 caracteres'),
+    body('apellido').isLength({ min: 3 }).withMessage('El apellido requiere masde 3 caracteres'),
+    check('edad').matches(/[0-9]{2}/).withMessage('La edad no es valida')
+  ],(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     miQuery(`INSERT INTO users (firstname, lastname, age) VALUES (
       ${ connection.escape(req.body.nombre) }, 
       ${ connection.escape(req.body.apellido) }, 
       ${ connection.escape(req.body.edad) });`, () => {
-      res.redirect('/usuarios')
-    })   
+      res.redirect('/usuarios')  
+      })
   })
 
   app.get('/usuarios/eliminar/:id', (req, res) => {
@@ -54,7 +66,20 @@ module.exports = function(app){
     })    
   })
 
-  app.post('/usuarios/modificar' , (req, res) => {
+  app.post('/usuarios/modificar' , [
+    check('id').matches(/[0-9]{4}?/).withMessage('El id no es valido'),
+    body('nombre').not().isEmpty().trim().escape(),
+    body('apellido').not().isEmpty().trim().escape(),
+    body('nombre').isLength({ min: 3 }).withMessage('El nombre requiere mas de 3 caracteres'),
+    body('apellido').isLength({ min: 3 }).withMessage('El apellido requiere masde 3 caracteres'),
+    check('edad').matches(/[0-9]{2}/).withMessage('La edad no es valida')
+  ] , (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     miQuery(`select * from users where id = ${connection.escape(req.body.id)}`, (user) => {
       let nombre;
       let apellido;
